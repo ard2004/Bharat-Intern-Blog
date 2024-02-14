cfaa0cc544df76e12d8a43506983baf1ae7a213f
captian-Arvoy <accofstardust@gmail.com>
2024-02-14 18:32:11 +0530

    todo list done

--- server.js
+++ server.js
@@ -1,17 +1,27 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
-var PORT = 5001 !== null && 5001 !== void 0 ? 5001 : process.env.PORT;
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
-var newItem = '';
-app.get('/', function (req, res) {
-    res.send("<h1>Hello world</h1>");
-});
-app.post('/', function (req, res) {
-    newItem = req.body.newItem;
-    res.redirect('/');
-});
-app.listen(PORT, function (req, res) {
-    console.log("listening on http://localhost:".concat(PORT, "/"));
-});
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+
+const PORT = 5001 ?? process.env.PORT
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+let newItems = []
+let newItem = ''
+app.get('/',(req,res) => {
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    let today = new Date();
+    let day = today.toLocaleDateString("en-US", options);
+    res.render("list",{kindOfDay: day, newListItems: newItems})
+})
+
+app.post('/',(req,res) => {
+    let newItem = req.body.newItem;
+    console.log(newItem)
+    newItems.push(newItem);
+    console.log(newItems)
+    res.redirect('/')
+})
+app.listen(PORT,(req,res)=>{
+    console.log(`listening on http://localhost:${PORT}/`)
+})
+
